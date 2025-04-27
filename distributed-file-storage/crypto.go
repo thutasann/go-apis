a83f02f9553fd6e424be0cb5efe61e55a4207eb9
thutasann <thutasann2002@gmail.com>
2025-04-27 13:57:23 +0800

    fix(dfs): encrypt block size

--- distributed-file-storage/crypto.go
+++ distributed-file-storage/crypto.go
@@ -34,15 +34,18 @@ func CopyEncrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
 	var (
 		buf    = make([]byte, 32*1024)
 		stream = cipher.NewCTR(block, iv)
+		nw     = block.BlockSize()
 	)
 
 	for {
 		n, err := src.Read(buf)
 		if n > 0 {
 			stream.XORKeyStream(buf, buf[:n])
-			if _, err := dst.Write(buf[:n]); err != nil {
+			nn, err := dst.Write(buf[:n])
+			if err != nil {
 				return 0, err
 			}
+			nw += nn
 		}
 
 		if err == io.EOF {
@@ -54,7 +57,7 @@ func CopyEncrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
 		}
 	}
 
-	return 0, nil
+	return nw, nil
 }
 
 // Copy Decrypt
