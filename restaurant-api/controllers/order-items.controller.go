f84779e4e7b7497ddb4f4a81292a3353050f961f
thutasann <thutasann2002@gmail.com>
2025-05-03 18:06:34 +0800

    feat(restaurant): controllers setup

--- restaurant-api/controllers/order-items.controller.go
+++ restaurant-api/controllers/order-items.controller.go
@@ -1,1 +1,23 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func GetOrderItem() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func GetOrderItemsByOrder() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func CreateOrderItem() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func UpdateOrderItem() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
--- restaurant-api/controllers/user.controller.go
+++ restaurant-api/controllers/user.controller.go
@@ -1,1 +1,27 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetUsers() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func SignUp() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func SignIn() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func hashPassword(password string) string {
+	return ""
+}
+
+func verifyPassword(userPassword string, providePassword string) (bool, string) {
+	return false, ""
+}
