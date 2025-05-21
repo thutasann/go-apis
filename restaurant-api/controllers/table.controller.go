d5986a8b9684aa1f579ea19731773cfd11a291e0
thutasann <thutasann2002@gmail.com>
2025-05-21 22:55:13 +0800

    feat(restaurant): update table

--- restaurant-api/controllers/table.controller.go
+++ restaurant-api/controllers/table.controller.go
@@ -13,6 +13,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Table Mongo Collection
@@ -96,13 +97,49 @@ func CreateTable() gin.HandlerFunc {
 // Update Table Controller
 func UpdateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		// defer cancel()
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
-		// var table models.Table
+		var table models.Table
 
-		// if err := c.BindJSON(&table); err != nil {
+		tableId := c.Param("table_id")
 
-		// }
+		if err := c.BindJSON(&table); err != nil {
+			helpers.Error(c, "JSON Bind Error", 0, err)
+			return
+		}
+
+		var updateObj primitive.D
+
+		if table.Number_of_guests != nil {
+			updateObj = append(updateObj, bson.E{Key: "number_of_guests", Value: table.Number_of_guests})
+		}
+
+		if table.Table_number != nil {
+			updateObj = append(updateObj, bson.E{Key: "table_number", Value: table.Table_number})
+		}
+
+		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		upsert := true
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		filter := bson.M{"table_id": tableId}
+
+		result, err := tableCollection.UpdateOne(
+			ctx, filter, bson.D{{Key: "$set", Value: updateObj}},
+			&opt,
+		)
+
+		if err != nil {
+			msg := fmt.Sprintf("Table update error: %s", err)
+			helpers.Error(c, msg, 0, err)
+			return
+		}
+
+		helpers.Success(c, "Update table success", result)
+
 	}
 }
