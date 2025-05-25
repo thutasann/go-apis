fb382b5f7f9fbf0be896debe42f80dda461b3bfc
thutasann <thutasann2002@gmail.com>
2025-05-25 17:13:12 +0800

    test(redis-from-scratch): parse command

--- redis-from-scratch/proto.go
+++ redis-from-scratch/proto.go
@@ -1,18 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
 
-// Command Struct
-type Command struct {
-	//??
+	"github.com/tidwall/resp"
+)
+
+const (
+	CommandSET = "SET" // SET Command
+)
+
+// Command Interface
+type Command interface{}
+
+// Set Command Struct
+type SetCommand struct {
+	key, val string
 }
 
 // Parse Command
-func parseCommand(msg string) (Command, error) {
-	t := msg[0]
-	switch t {
-	case '*':
-		fmt.Println(msg[1:])
+func parseCommand(raw string) (Command, error) {
+	rd := resp.NewReader(bytes.NewBufferString(raw))
+
+	for {
+		v, _, err := rd.ReadValue()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Read %s\n", v.Type())
+
+		if v.Type() == resp.Array {
+			for _, value := range v.Array() {
+				switch value.String() {
+				case CommandSET:
+					fmt.Printf("%+v\n", v.Array())
+					if len(v.Array()) != 3 {
+						return nil, fmt.Errorf("invalid number of variables for SET command")
+					}
+					cmd := SetCommand{
+						key: v.Array()[1].String(),
+						val: v.Array()[2].String(),
+					}
+					return cmd, nil
+				default:
+					fmt.Println("no value type matched")
+				}
+			}
+		}
 	}
-	return Command{}, nil
+
+	return "foo", nil
 }
