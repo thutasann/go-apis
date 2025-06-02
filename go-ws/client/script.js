def32c74b07b35fc6a59d2bfb2357f60ebe358c2
thutasann <thutasann2002@gmail.com>
2025-06-03 01:55:45 +0800

    feat(go-ws): socket events

--- go-ws/client/script.js
+++ go-ws/client/script.js
@@ -1,10 +1,66 @@
 // @ts-check
 
-/** @type { string } */
+/**
+ * @typedef {'new_message' | 'delete_message'} SocketEventType
+ */
+
+/**
+ * @typedef {'send_message'} SendEvent
+ */
+
+/** selecte chat @type { string }  */
 let selectedChat = 'general';
 
-/** @type { WebSocket | null} */
+/** ws connection @type { WebSocket | null} */
 let conn = null;
+
+/**
+ * Socket Events
+ */
+class SocketEvent {
+  /**
+   * Socket Event
+   * @param {string} type - Socket Event Type
+   * @param {*} payload - Socket Event payload
+   */
+  constructor(type = '', payload = null) {
+    this.type = type;
+    this.payload = payload;
+  }
+}
+
+/**
+ * Route Event
+ * @param {{type: SocketEventType | undefined}} event - socket event
+ */
+function routeEvent(event) {
+  if (event.type === undefined) {
+    alert('no type field in the socket event');
+  }
+
+  switch (event.type) {
+    case 'new_message':
+      console.log('🚀 new message...');
+      break;
+    default:
+      alert('unsupported message type');
+      break;
+  }
+}
+
+/**
+ * send socket event
+ * @param {SendEvent} eventName - socket event name
+ * @param {*} payload - socket payload
+ */
+function sendEvent(eventName, payload) {
+  if (!conn) {
+    alert('socket connection failed...');
+    return;
+  }
+  const event = new SocketEvent(eventName, payload);
+  conn.send(JSON.stringify(event));
+}
 
 /**
  * Change Chat Room
@@ -23,7 +79,7 @@ function changeChatRoom() {
 function sendMessage() {
   const newMessage = /** @type {HTMLInputElement | null} */ (document.getElementById('message'));
   if (newMessage != null && conn) {
-    conn.send(newMessage.value);
+    sendEvent('send_message', newMessage.value);
   }
   return false;
 }
@@ -49,7 +105,9 @@ window.onload = function () {
     conn = new WebSocket('ws://' + document.location.host + '/ws');
 
     conn.onmessage = function (evt) {
-      console.log('ℹ️ Event --> ', evt.data);
+      const eventData = JSON.parse(evt.data);
+      const event = Object.assign(new SocketEvent(), eventData);
+      routeEvent(event);
     };
   } else {
     alert('Browser does not support Websocket');
---
+++ go-ws/event.go
@@ -0,0 +1,28 @@
+package main
+
+import "encoding/json"
+
+// Event represents the socket event
+type Event struct {
+	Type    string          `json:"type"`    // event type
+	Payload json.RawMessage `json:"payload"` // event payload
+}
+
+// EventHandler defines a function type that handles an Event from a client.
+//
+// It takes two parameters:
+//   - event: the Event object representing the incoming data or action.
+//   - c: a pointer to the Client that sent the event.
+//
+// It returns an error if the event handling fails.
+type EventHandler func(event Event, c *Client) error
+
+const (
+	EventSendMesasge = "send_message"
+)
+
+// Send Message Event represents the `send_message` socket event
+type SendMessageEvent struct {
+	Message string `json:"message"`
+	From    string `json:"from"`
+}
