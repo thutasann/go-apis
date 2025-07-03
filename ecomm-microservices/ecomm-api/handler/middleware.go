a5caf5da8d03153d0fe0e96c6041c4a68810bce4
thutasann <thutasann2002@gmail.com>
2025-07-03 20:55:40 +0800

    feat(ecom-ms): apply middlewares in routes

--- ecomm-microservices/ecomm-api/handler/middleware.go
+++ ecomm-microservices/ecomm-api/handler/middleware.go
@@ -11,7 +11,7 @@ import (
 
 type authKey struct{}
 
-func GetAuthMiddleware(tokenMaker *token.JWTMaker) func(http.Handler) http.Handler {
+func GetAuthMiddlewareFunc(tokenMaker *token.JWTMaker) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			claims, err := verifyClaimsFromAuthHeader(r, tokenMaker)
