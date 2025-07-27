173c21cd8cfb8aa7911af16ac6bf9c73cbdb7dcc
thutasann <thutasann2002@gmail.com>
2025-07-27 14:33:31 +0800

    add(gowebrtc): makefile scripts and docker files

--- go-webrtc/internal/handlers/room.go
+++ go-webrtc/internal/handlers/room.go
@@ -1,24 +1,56 @@
 package handlers
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
+	gguid "github.com/google/uuid"
+	"github.com/thuta/gowebrtc/pkg/webrtc"
 )
 
 func RoomCreate(c *fiber.Ctx) error {
-	return nil
+	return c.Redirect(fmt.Sprintf("/room/%s", gguid.New().String()))
 }
 
 func Room(c *fiber.Ctx) error {
-	return nil
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		c.Status(400)
+		return nil
+	}
+
+	ws := "ws"
+	if os.Getenv("ENVIRONMENT") == "PRODUCTION" {
+		ws = "wss"
+	}
+
+	uuid, suuid, _ := createOrGetRoom(uuid)
+	return c.Render("peer", fiber.Map{
+		"RoomWebsocketAddr":   fmt.Sprintf("%s://%s/room/%s/websocket", ws, c.Hostname(), uuid),
+		"RoomLink":            fmt.Sprintf("%s://%s/room/%s", c.Protocol(), c.Hostname(), uuid),
+		"ChatWebsocketAddr":   fmt.Sprintf("%s://%s/room/%s/chat/websocket", ws, c.Hostname(), uuid),
+		"ViewerWebsocketAddr": fmt.Sprintf("%s://%s/room/%s/viewer/websocket", ws, c.Hostname(), uuid),
+		"StreamLink":          fmt.Sprintf("%s://%s/stream/%s", c.Protocol(), c.Hostname(), suuid),
+		"Type":                "room",
+	}, "layouts/main")
 }
 
 func RoomChat(c *fiber.Ctx) error {
 	return nil
 }
 
-func RoomWebSocket(c *websocket.Conn) {}
+func RoomWebSocket(c *websocket.Conn) {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return
+	}
+	_, _, room := createOrGetRoom(uuid)
+}
 
 func RoomChatWebsocket(c *websocket.Conn) {}
 
 func RoomViewerWebsocket(c *websocket.Conn) {}
+
+func createOrGetRoom(uuid string) (string, string, *webrtc.Room) {}
