1519520d65817495dfa3f8e3dc584eb3b5c969e7
thutasann <thutasann2002@gmail.com>
2026-01-21 23:44:22 +0800

    feat: train agent

--- concurrent-transport-platform/cmd/train-agent/main.go
+++ concurrent-transport-platform/cmd/train-agent/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/thutasann/ctp/internal/shutdown"
+	"github.com/thutasann/ctp/internal/train"
+	"github.com/thutasann/ctp/pkg/logx"
+)
 
 func main() {
-	fmt.Println("train-agent starting (skeleton)")
+	rand.Seed(time.Now().UnixNano())
+
+	ctx := shutdown.WithSignal(context.Background())
+
+	logger := logx.New("TRAIN")
+
+	stationAddr := "localhost:8000"
+	stations := []string{"Bugis", "CityHall", "Raffles"}
+
+	trainCount := 100
+
+	for i := 0; i < trainCount; i++ {
+		trainID := fmt.Sprintf("EW-%03d", i)
+		station := stations[rand.Intn(len(stations))]
+
+		go train.RunTrain(ctx, stationAddr, trainID, station, logger)
+	}
+
+	<-ctx.Done()
 }
--- concurrent-transport-platform/internal/train/client.go
+++ concurrent-transport-platform/internal/train/client.go
@@ -1,1 +1,49 @@
 package train
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/thutasann/ctp/internal/network"
+	"github.com/thutasann/ctp/internal/telemetry"
+)
+
+func RunTrain(
+	ctx context.Context,
+	addr string,
+	trainID string,
+	station string,
+	logger *log.Logger,
+) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		conn, err := network.Dial(ctx, addr)
+		if err != nil {
+			logger.Println(trainID, "dail failed")
+			time.Sleep(time.Second)
+			continue
+		}
+
+		// simulate session
+		sessionDuration := time.Duration(rand.Intn(10)+5) * time.Second
+		end := time.Now().Add(sessionDuration)
+
+		for time.Now().Before(end) {
+			t := Generate(trainID, station)
+			_ = telemetry.Encode(conn, t)
+
+			time.Sleep(time.Duration(rand.Intn(500)+500) * time.Millisecond)
+		}
+
+		conn.Close()
+		// simulate disconnect
+		time.Sleep(time.Duration(rand.Intn(3)+1) * time.Second)
+	}
+}
