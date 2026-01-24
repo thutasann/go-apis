5561d169f3d7d2acc32fefc5d60e93d3395790ef
thutasann <thutasann2002@gmail.com>
2026-01-24 16:11:01 +0800

    feat: creating the player

--- go-snake-game/main.go
+++ go-snake-game/main.go
@@ -1,19 +1,43 @@
 package main
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
 
 const (
 	screenWidth  = 640
 	screenHeight = 480
+	gridSize     = 20
 )
 
-type Game struct{}
+type Point struct {
+	x, y int
+}
+
+type Game struct {
+	snake []Point
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	for _, p := range g.snake {
+		vector.FillRect(
+			screen,
+			float32(p.x*gridSize),
+			float32(p.y*gridSize),
+			gridSize,
+			gridSize,
+			color.White,
+			true,
+		)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -22,7 +46,23 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 // Building a Snake Game with Go and Ebiten
 func main() {
-	g := &Game{}
+	g := &Game{
+		snake: []Point{
+			{
+				x: screenWidth / gridSize / 2,
+				y: screenHeight / gridSize / 2,
+			},
+			{
+				x: screenWidth/gridSize/2 - 1,
+				y: screenHeight / gridSize / 2,
+			},
+		},
+	}
 
-	ebiten.RunGame(g)
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Go Snake")
+
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
 }
