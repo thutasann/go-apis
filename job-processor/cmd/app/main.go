354a9ee62d747712b0e3014884fa6c02ad3e675f
thutasann <thutasann2002@gmail.com>
2026-02-15 00:48:31 +0800

    feat: queue

--- job-processor/cmd/app/main.go
+++ job-processor/cmd/app/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/thutasann/job-processor/internal/job"
+	"github.com/thutasann/job-processor/internal/queue"
 )
 
 func main() {
-	j := job.New("1", job.High, func() error {
-		fmt.Println("Executing job 1")
-		return nil
-	})
+	q := queue.New(2, 2)
 
-	fmt.Println(j)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// enqueue
+	q.Enqueue(job.New("1", job.Normal, nil))
+	q.Enqueue(job.New("2", job.High, nil))
+	q.Enqueue(job.New("3", job.Normal, nil))
+
+	// dequeue
+	for i := 0; i < 3; i++ {
+		j, _ := q.Dequeue(ctx)
+		fmt.Println("Dequeued:", j)
+	}
+
+	time.Sleep(time.Second)
 }
--- job-processor/internal/queue/queue.go
+++ job-processor/internal/queue/queue.go
@@ -1,1 +1,57 @@
 package queue
+
+import (
+	"context"
+
+	"github.com/thutasann/job-processor/internal/job"
+)
+
+// Queue holds two buffered channels for priority handling
+type Queue struct {
+	highCh   chan *job.Job
+	normalCh chan *job.Job
+}
+
+// New creates a new priority queue.
+// highSize and normalSize define buffer capacity.
+func New(highSize, normalSize int) *Queue {
+	return &Queue{
+		highCh:   make(chan *job.Job, highSize),
+		normalCh: make(chan *job.Job, normalSize),
+	}
+}
+
+// Enqueue inserts a job into the appropriate buffer.
+// this BLOCKS when the buffer is full (intentional backpressure)
+func (q *Queue) Enqueue(j *job.Job) {
+	switch j.Priority {
+	case job.High:
+		q.highCh <- j
+	default:
+		q.normalCh <- j
+	}
+}
+
+// Dequeue selects a job with priority awareness.
+// It respects context cancellation for graceful shutdown.
+func (q *Queue) Dequeue(ctx context.Context) (*job.Job, error) {
+	for {
+		// First, try to pull HIGH without blocking.
+		select {
+		case j := <-q.highCh:
+			return j, nil
+		default:
+		}
+
+		// If no HIGH job immediately available,
+		// block waiting for either HIGH or NORMAL.
+		select {
+		case j := <-q.highCh:
+			return j, nil
+		case j := <-q.normalCh:
+			return j, nil
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		}
+	}
+}
