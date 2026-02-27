230ffad6c5009b941924a8292d90fe069863b18b
Thuta Sann <thutasann2002@gmail.com>
2026-02-27 23:16:37 +0800

    test: component import

--- go-react-ssr-engine/pages/about.tsx
+++ go-react-ssr-engine/pages/about.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Button from '../components/button';
 
 export default function About(props) {
   return (
     <div>
-      <h1>About</h1>
+      <h1>About Page</h1>
       <p>A Go-powered React SSR engine.</p>
       <a href='/'>Home</a>
+      <Button />
     </div>
   );
 }
