1f01c7bc6e0ae5bc2e697111a4ec34a7d624f9f7
Thuta Sann <thutasann2002@gmail.com>
2026-03-12 22:46:22 +0800

    add: mutex

--- advanced-go-course/cmd/concurrency/basic_concurrency/main.go
+++ advanced-go-course/cmd/concurrency/basic_concurrency/main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(3)
-
-	orders := generateOrders(20)
-
-	go func() {
-		defer wg.Done()
-		processOrders(orders)
-	}()
-
-	go func() {
-		defer wg.Done()
-		updateOrderStatuses(orders)
-	}()
-
-	go func() {
-		defer wg.Done()
-		reportOrderStatus(orders)
-	}()
-
-	wg.Wait()
-
-	fmt.Println("All operations completed. Existing")
+	Order_Example()
 }
--- advanced-go-course/cmd/concurrency/basic_concurrency/order_example.go
+++ advanced-go-course/cmd/concurrency/basic_concurrency/order_example.go
@@ -3,18 +3,25 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 type Order struct {
 	ID     int
 	Status string
+	mu     sync.Mutex
 }
+
+var (
+	totalUpdates int
+	updateMutex  sync.Mutex
+)
 
 func generateOrders(count int) []*Order {
 	orders := make([]*Order, count)
 
-	for i := 0; i < count; i++ {
+	for i := range count {
 		orders[i] = &Order{
 			ID:     i + 1,
 			Status: "Pending",
@@ -33,28 +40,68 @@ func processOrders(orders []*Order) {
 	}
 }
 
-func updateOrderStatuses(orders []*Order) {
-	for _, order := range orders {
-		time.Sleep(time.Duration(rand.Intn(300)) * time.Millisecond)
-		status := []string{"Processing", "Shipped", "Delivered"}[rand.Intn(3)]
-		order.Status = status
-		fmt.Printf(
-			"Updated order %d status: %s\n", order.ID, status,
-		)
-	}
+// func updateOrderStatuses(orders []*Order) {
+// 	for _, order := range orders {
+// 		time.Sleep(time.Duration(rand.Intn(300)) * time.Millisecond)
+// 		status := []string{"Processing", "Shipped", "Delivered"}[rand.Intn(3)]
+// 		order.Status = status
+// 		fmt.Printf(
+// 			"Updated order %d status: %s\n", order.ID, status,
+// 		)
+// 	}
+// }
+
+func updateOrderStatus(order *Order) {
+	order.mu.Lock()
+	time.Sleep(time.Duration(rand.Intn(300)) * time.Millisecond)
+	status := []string{"Processing", "Shipped", "Delivered"}[rand.Intn(3)]
+	order.Status = status
+	fmt.Printf(
+		"Updated order %d status: %s\n", order.ID, status,
+	)
+	order.mu.Unlock()
+
+	updateMutex.Lock()
+	defer updateMutex.Unlock()
+	currentUpdates := totalUpdates
+	time.Sleep(5 * time.Millisecond)
+	totalUpdates = currentUpdates + 1
 }
 
 func reportOrderStatus(orders []*Order) {
-	for range 5 {
-		time.Sleep(1 * time.Second)
-		fmt.Println("\n--- Order status report ---")
-		for _, order := range orders {
-			fmt.Printf("Order %d: %s\n", order.ID, order.Status)
-		}
-		fmt.Println("-----------------------------------------")
+	fmt.Println("\n--- Order status report ---")
+	for _, order := range orders {
+		fmt.Printf("Order %d: %s\n", order.ID, order.Status)
 	}
+	fmt.Println("-----------------------------------------")
 }
 
 func Order_Example() {
+	var wg sync.WaitGroup
+	wg.Add(3)
 
+	orders := generateOrders(20)
+
+	// process orders goroutine
+	// go func() {
+	// 	defer wg.Done()
+	// 	processOrders(orders)
+	// }()
+
+	// update order statuses goroutine
+	for range 3 {
+		go func() {
+			defer wg.Done()
+			for _, order := range orders {
+				updateOrderStatus(order)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	reportOrderStatus(orders)
+
+	fmt.Println("All operations completed. Existing")
+	fmt.Println("totalUpdates --> ", totalUpdates)
 }
