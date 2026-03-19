9b9ccc46765d2e55eb5c3609c68b287dcd9d202e
Thuta Sann <thutasann2002@gmail.com>
2026-03-19 22:07:46 +0800

    add: iota

---
+++ advanced-go-course/cmd/core/concepts/iota.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const (
+	_ = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturaday
+	Sunday
+)
+
+func IOTA_SAMPLE() {
+	fmt.Println(Monday)
+	fmt.Println(Tuesday)
+	fmt.Println(Wednesday)
+	fmt.Println(Thursday)
+	fmt.Println(Friday)
+	fmt.Println(Saturaday)
+	fmt.Println(Sunday)
+}
