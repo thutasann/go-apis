335be846e1c9c621b2664dbd304c68bb579785a5
Thuta Sann <thutasann2002@gmail.com>
2026-03-23 23:16:32 +0800

    feat: order svc handlers

--- go-grpc-ms/services/orders/http.go
+++ go-grpc-ms/services/orders/http.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	handler "github.com/thutasann/go-grpc-ms/services/orders/handler/orders"
+	"github.com/thutasann/go-grpc-ms/services/orders/service"
+)
+
+type httpServer struct {
+	addr string
+}
+
+func NewHttpServer(addr string) *httpServer {
+	return &httpServer{
+		addr: addr,
+	}
+}
+
+func (s *httpServer) Run() error {
+	router := http.NewServeMux()
+	orderService := service.NewOrderService()
+	orderHandler := handler.NewHttpOrdersHandler(orderService)
+	orderHandler.RegisterRouter(router)
+
+	log.Println("Starting server on", s.addr)
+	return http.ListenAndServe(s.addr, router)
+}
--- go-grpc-ms/services/orders/main.go
+++ go-grpc-ms/services/orders/main.go
@@ -2,6 +2,9 @@ package main
 
 // Go + gRPC
 func main() {
+	httpServer := NewHttpServer(":8000")
+	httpServer.Run()
+
 	grpcServer := NewGRPCServer(":9000")
 	grpcServer.Run()
 }
