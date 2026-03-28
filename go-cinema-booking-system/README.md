f1cef27be9611b9c11f80fa9f97198c1f02e85e4
Thuta Sann <thutasann2002@gmail.com>
2026-03-28 16:56:55 +0800

    feat: concurrent_store

--- go-cinema-booking-system/README.md
+++ go-cinema-booking-system/README.md
@@ -1,1 +1,7 @@
 # Building Cinema Booking System
+
+## Unit Tests
+
+```bash
+go test ./... -v -count=1 -race
+```
---
+++ go-cinema-booking-system/internal/booking/concurrent_store.go
@@ -0,0 +1,39 @@
+package booking
+
+import "sync"
+
+type ConcurrentStore struct {
+	bookings map[string]Booking // "A-1" -> booking
+	sync.RWMutex
+}
+
+func NewConcurrentStore() *ConcurrentStore {
+	return &ConcurrentStore{
+		bookings: map[string]Booking{},
+	}
+}
+
+func (s *ConcurrentStore) Book(b Booking) error {
+	s.Lock()
+	defer s.Unlock()
+
+	if _, exists := s.bookings[b.SeatID]; exists {
+		return ErrSeatAlreadyBooked
+	}
+
+	s.bookings[b.SeatID] = b
+	return nil
+}
+
+func (s *ConcurrentStore) ListBookings(movieID string) []Booking {
+	s.RLock()
+	defer s.RUnlock()
+
+	var result []Booking
+	for _, b := range s.bookings {
+		if b.MovieID == movieID {
+			result = append(result, b)
+		}
+	}
+	return result
+}
