69113ca808a9049e3db56e0cd6da9a964569c982
Thuta Sann <thutasann2002@gmail.com>
2026-04-12 01:05:41 +0800

    feat: game_client init

--- go-low-latency-game-server/game_client/main.go
+++ go-low-latency-game-server/game_client/main.go
@@ -1,5 +1,60 @@
 package main
 
-func main() {
+import (
+	"log"
+	"math"
+	"math/rand"
 
+	"github.com/gorilla/websocket"
+)
+
+const wsServerEndpoint = "ws://localhost:4000/ws"
+
+// Login Payload
+type Login struct {
+	ClientID int    `json:"clientID"`
+	Username string `json:"username"`
+}
+
+// Game Client
+type GameClient struct {
+	conn     *websocket.Conn
+	clientID int
+	username string
+}
+
+// Initialize New Game Client
+func newGameClient(conn *websocket.Conn, username string) *GameClient {
+	return &GameClient{
+		conn:     conn,
+		clientID: rand.Intn(math.MaxInt),
+		username: username,
+	}
+}
+
+func (c *GameClient) login() error {
+	return c.conn.WriteJSON(Login{
+		ClientID: c.clientID,
+		Username: c.username,
+	})
+}
+
+// game_client
+func main() {
+	dialer := websocket.Dialer{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	conn, _, err := dialer.Dial(wsServerEndpoint, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := newGameClient(conn, "James")
+
+	if err := c.login(); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ go-low-latency-game-server/go.mod
@@ -0,0 +1,5 @@
+module github.com/thutasann/gogameserver
+
+go 1.25.0
+
+require github.com/gorilla/websocket v1.5.3 // indirect
