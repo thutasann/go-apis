473c3c712ac8d8e156c3ebe3822a5820c63c1c82
Thuta Sann <thutasann2002@gmail.com>
2026-04-26 23:23:16 +0800

    fix: wall

--- go-terminal-game/main.go
+++ go-terminal-game/main.go
@@ -6,25 +6,42 @@ import (
 )
 
 func main() {
-	width := 20
-	height := 20
+	width := 80
+	height := 18
 	level := make([][]byte, height)
 
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
+	for h := range height {
+		for range width {
 			level[h] = make([]byte, width)
 		}
 	}
 
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
-			level[h][w] = WALL
+	for h := range height {
+		for w := range width {
+			if h == 0 {
+				level[h][w] = WALL
+			}
+
+			if w == 0 {
+				level[h][w] = WALL
+			}
+
+			if w == width-1 {
+				level[h][w] = WALL
+			}
+
+			if h == height-1 {
+				level[h][w] = WALL
+			}
 		}
 	}
 
 	buf := new(bytes.Buffer)
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
+	for h := range height {
+		for w := range width {
+			if level[h][w] == NOTHING {
+				buf.WriteString(" ")
+			}
 			if level[h][w] == WALL {
 				buf.WriteString("H")
 			}
